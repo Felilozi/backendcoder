e0779aeade0e25c7096fda18317c71592993828f
felilozi <felicialozinski97@gmail.com>
2023-09-17 22:02:23 -0300

    preEntrega2

---
+++ src/ParaEstudiar/estudiando2.js
@@ -0,0 +1,29 @@
+// const persona = {
+//     nombre: "Juan",
+//     edad: 30,
+//     ciudad:'Ciudad Madero'
+// }
+// const entradas = Object.entries(persona);
+// const clave = Object.keys(persona);
+// const valor = Object.values(persona);
+
+// console.log(entradas);
+// console.log(clave);
+// console.log(valor);
+
+// const promesa = new Promise((resolve,reject)=>{
+//     setTimeout(()=>{
+//         resolve('Funciona bien ');
+//     },2000);
+
+// });
+// promesa
+// .then((resultado)=>{
+//     console.log(`Exito ${resultado}`);
+// })
+// .catch((error)=>{
+//     console.log(error);
+// })
+// .finally(()=>{
+//     console.log('Este bloque ejecutara siempre ya sea que eaxista un exito o un error ')
+// });
