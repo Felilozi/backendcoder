1d448d57ab16285d76e294a3b927a837e708cdfe
felilozi <felicialozinski97@gmail.com>
2024-01-29 11:17:04 -0300

    Agregando looger

---
+++ src/commons/loggerDev.js
@@ -0,0 +1,58 @@
+import winston from 'winston';
+import fs from 'fs';
+
+const logsDir = 'logs';
+if (!fs.existsSync(logsDir)) {
+    fs.mkdirSync(logsDir);
+}
+
+const logFormat = winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.simple()
+);
+
+const levels = {
+    fatal: 0,
+    error: 1,
+    warning: 2,
+    info: 3,
+    http: 4,
+    debug: 5
+};
+
+const colors = {
+    fatal: 'red',
+    error: 'red',
+    warning: 'yellow',
+    info: 'green',
+    http: 'green',
+    debug: 'blue'
+};
+
+winston.addColors(colors);
+
+const loggerDev = winston.createLogger({
+    levels,
+    format: logFormat,
+    transports: [
+        new winston.transports.Console({
+            format: winston.format.combine(
+                winston.format.colorize({ all: true }),
+                winston.format.simple()
+            )
+        }),
+        new winston.transports.File({ filename: 'logs/app-dev.log' })
+    ]
+
+});
+loggerDev.level = 'debug'; // O 'http' según el nivel que quieras ver
+const logTestErrorsdev = () => {
+    loggerDev.fatal('This is a test fatal error');
+    loggerDev.error('This is a test error');
+    loggerDev.warning('This is a test warning');
+    loggerDev.info('This is a test info');
+    loggerDev.http('This is a test http');
+    loggerDev.debug('This is a test debug');
+};
+
+export {loggerDev, logTestErrorsdev} ;
--- src/server.js
+++ src/server.js
@@ -15,8 +15,14 @@ import MongoStorage from "connect-mongo"
 import { config } from './config.js'
 import passport from 'passport'
 import initializedPassport from './controller/passportController.js';
+import fs from 'fs';
+import {loggerDev, logTestErrorsdev} from './commons/loggerDev.js'
+import {loggerProD, logTestErrorsPro} from './commons/logerProd.js'
+
+
 const server = express()
 
+const errorLogStream = fs.createWriteStream('logs/error.log', { flags: 'a' });
 //mill 
 const swaggerDocument = YAML.load('./openapi.yml')
 server.use(json())
@@ -25,7 +31,7 @@ server.use(cookieParser('CookieSecret'))
 server.use(session({
     secret: 'SECRETCODE',
     resave: true,
-    saveUninitialized:true
+    saveUninitialized: true
     //Puede guardar seccion  o guardar cualquier dato  garatiza el dato  
 }))
 const mongooseOptions = {
@@ -38,11 +44,11 @@ server.use(session({
         mongoUrl: config.MONGODB_URI,
         mongoOptions: mongooseOptions
     }),
-    secret:'CODER_SECRET',
-    resave:false,
-    saveUninitialized:false
+    secret: 'CODER_SECRET',
+    resave: false,
+    saveUninitialized: false
 }))
-;
+    ;
 server.engine(
     '.hbs',
     engine({
@@ -58,7 +64,7 @@ console.log(db)
 
 server.use('/api-doc', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 server.use('/api', router)
-server.use('/feli',routerPrueba)
+server.use('/feli', routerPrueba)
 
 function shutdown(message, code) {
     console.log(`Server ${code ? `${message}: ${code}` : 'stopped'}`)
@@ -68,6 +74,29 @@ process.on('exit', code => shutdown('About to exit with', code))
 
 initializedPassport()
 server.use(passport.initialize())
+
+
+
+server.use((err, req, res, next) => {
+    loggerProD.error(err.stack); // Using the production logger for error logs
+    errorLogStream.write(`${new Date().toISOString()} - ${err.stack}\n`);
+    next(err);
+});
+
 // server.use(passport.session())
+server.get('/loggerTest', (req, res) => {
+    try {
+        // Simulate an error for testing
+        logTestErrorsdev();
+        // logTestErrorsPro();
+        // throw new Info('This is a test error');
+    } catch (error) {
+        // Log the error using the appropriate logger based on environment
+        const logger = process.env.NODE_ENV === 'production' ? loggerProD : loggerDev;
+        logger.error('Error here'); // Replace this line with the desired error message
+        logger.error(error.message);
+        res.status(500).send('Internal Server Errorjjjj');
+    }
+});
 
 export default server
